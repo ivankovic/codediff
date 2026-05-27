5026a47ecb591afaee388394998d0c194808ee3e
Marko Ivankovic <marko@ivankovic.me>
2026-05-27 19:36:19 +0200

    human: remove vibe-coded app

--- src/tui/mod.rs
+++
@@ -1,9 +0,0 @@
-// TUI module exports
-pub mod app;
-pub mod ui;
-pub mod widgets;
-
-#[cfg(test)]
-mod app_tests;
-#[cfg(test)]
-mod ui_tests;
--- src/tui/widgets/mod.rs
+++
@@ -1,6 +0,0 @@
-// Widget module exports
-pub mod code_panel;
-pub mod ast_popup;
-
-#[cfg(test)]
-mod widgets_tests;
