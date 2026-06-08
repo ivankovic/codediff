f3c8cb86de978a16fa135696b2998cc59c48de3e
Marko Ivankovic <marko@ivankovic.me>
2026-06-08 15:02:42 +0200

    more optimal solutions

--- src/test/optimal_solutions.rs
+++ src/test/optimal_solutions.rs
@@ -81,6 +81,44 @@ mod tests {
     }
 
     #[test]
+    fn hello_world_removed_message() -> Result<()> {
+        let test_diffs = test::helper::handmade_test_code_pairs()?;
+        let (before, after) = test_diffs
+            .get("hello-world-removed-message")
+            .unwrap()
+            .clone();
+
+        let diff = diff::diff_code(&before, &after);
+
+        assert!(diff.ast.is_some());
+
+        let diff_ast = diff.ast.unwrap();
+        let before_ast = before.ast.unwrap();
+        let after_ast = after.ast.unwrap();
+
+        let before_root = before_ast.root_node();
+        let after_root = after_ast.root_node();
+
+        let path = vec!["function_item"];
+        let mapping =
+            test::helper::mapping_for_path(&path, &path, before_root, after_root, &diff_ast)?;
+        assert_eq!(mapping.operation, ASTMappingOperation::MatchButNotIdentical);
+
+        let path = vec!["function_item", "block", "expression_statement:1"];
+        let mapping =
+            test::helper::mapping_for_path(&path, &path, before_root, after_root, &diff_ast)?;
+        assert_eq!(mapping.operation, ASTMappingOperation::Identical);
+
+        assert!(test::helper::was_tree_deleted(
+            &["function_item", "block", "expression_statement:2"],
+            before_root,
+            &diff_ast
+        )?);
+
+        Ok(())
+    }
+
+    #[test]
     fn rust_hash_optimization() -> Result<()> {
         let test_diffs = test::helper::handmade_test_code_pairs()?;
         let (before, after) = test_diffs.get("rust-hash-optimization").unwrap().clone();
