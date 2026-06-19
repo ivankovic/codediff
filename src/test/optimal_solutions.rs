005419b20430085d34988d1116c67dc154c67a76
Marko Ivankovic <marko@ivankovic.me>
2026-06-19 11:19:35 +0200

    added if in rust to optimal solutions

--- src/test/optimal_solutions.rs
+++ src/test/optimal_solutions.rs
@@ -773,4 +773,76 @@ mod tests {
 
         Ok(())
     }
+
+    #[test]
+    fn rust_add_if() -> Result<()> {
+        let test_diffs = test::helper::handmade_test_code_pairs()?;
+        let (before, after) = test_diffs.get("rust-add-if").unwrap().clone();
+
+        let diff = diff::diff_code(&before, &after);
+
+        assert!(diff.ast.is_some());
+
+        let diff_ast = diff.ast.unwrap();
+        let before_ast = before.ast.unwrap();
+        let after_ast = after.ast.unwrap();
+
+        let before_root = before_ast.root_node();
+        let after_root = after_ast.root_node();
+
+        // Root nodes should be mapped
+        assert!(
+            diff_ast
+                .mapping
+                .contains_key(&(before_root.id(), after_root.id()))
+        );
+
+        // This test case starts with an "if-else" code, and adds a new condition to the *start* of
+        // the if, going to "if-else if-else" code. In the Rust AST, what happens is that the entire
+        // before if_expression block becomes the child of the else node of the newly added
+        // if_expression block.
+
+        let if_expression_before = test::helper::node_for_path(
+            before_root,
+            &[
+                "function_item",
+                "block",
+                "expression_statement",
+                "if_expression",
+            ],
+        )?;
+
+        let path = vec![
+            "function_item",
+            "block",
+            "expression_statement",
+            "if_expression",
+        ];
+        assert!(
+            test::helper::was_node_added(&path, after_root, &diff_ast)?,
+            "The added if_expression is not mapped as added"
+        );
+
+        let outer_if_expression_after = test::helper::node_for_path(after_root, &path)?;
+
+        let inner_if_expression_after = test::helper::node_for_path(
+            outer_if_expression_after,
+            &["else_clause", "if_expression"],
+        )?;
+
+        assert!(
+            diff_ast
+                .mapping
+                .contains_key(&(if_expression_before.id(), inner_if_expression_after.id())),
+            "The if_expression from before is not correctly mapped as the child of the else_clause of the added if"
+        );
+
+        let mapping = diff_ast
+            .mapping
+            .get(&(if_expression_before.id(), inner_if_expression_after.id()))
+            .unwrap();
+        assert_eq!(mapping.operation, ASTMappingOperation::Identical);
+
+        Ok(())
+    }
 }
