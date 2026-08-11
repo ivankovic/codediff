180a97fcc62c196fde9c7060b40c1ae945938fe3
Marko Ivankovic <marko@ivankovic.me>
2026-08-12 00:58:42 +0200

    partial optimal solutions

--- src/test/optimal_solutions/small/json_excalidraw_excalidraw_change_translations_mostly_add.rs
+++ src/test/optimal_solutions/small/json_excalidraw_excalidraw_change_translations_mostly_add.rs
@@ -21,5 +21,7 @@ use crate::test;
 
 #[test]
 fn optimal_solution() -> Result<()> {
-    test::helper::human_mapping::assert_matches_human_mapping("json-excalidraw-excalidraw-change-translations-mostly-add")
+    test::helper::human_mapping::assert_matches_human_mapping(
+        "json-excalidraw-excalidraw-change-translations-mostly-add",
+    )
 }
--- src/test/optimal_solutions/small/lua_neovim_neovim_logic_change_with_some_code_re_use.rs
+++ src/test/optimal_solutions/small/lua_neovim_neovim_logic_change_with_some_code_re_use.rs
@@ -21,5 +21,7 @@ use crate::test;
 
 #[test]
 fn optimal_solution() -> Result<()> {
-    test::helper::human_mapping::assert_matches_human_mapping("lua-neovim-neovim-logic-change-with-some-code-re-use")
+    test::helper::human_mapping::assert_matches_human_mapping(
+        "lua-neovim-neovim-logic-change-with-some-code-re-use",
+    )
 }
