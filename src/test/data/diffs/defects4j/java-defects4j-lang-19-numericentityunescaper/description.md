a2cc683f8a87ec72576cde6af77273cbe18611cd
Marko Ivankovic <marko@ivankovic.me>
2026-09-15 10:49:03 +0200

    many many defects4j solutions

---
+++ src/test/data/diffs/defects4j/java-defects4j-lang-19-numericentityunescaper/description.md
@@ -0,0 +1,1 @@
+Requires N:M mapping.
--- src/test/fixtures/defects4j.rs
+++ src/test/fixtures/defects4j.rs
@@ -50,9 +50,15 @@ mod java_defects4j_chart_20_valuemarker;
 #[cfg(test)]
 mod java_defects4j_chart_21_defaultboxandwhiskercategorydataset;
 #[cfg(test)]
+mod java_defects4j_chart_24_graypaintscale;
+#[cfg(test)]
 mod java_defects4j_chart_2_datasetutilities;
 #[cfg(test)]
 mod java_defects4j_chart_4_xyplot;
+#[cfg(test)]
+mod java_defects4j_chart_6_shapelist;
+#[cfg(test)]
+mod java_defects4j_cli_12_gnuparser;
 #[cfg(test)]
 mod java_defects4j_cli_13_writeablecommandline;
 #[cfg(test)]
@@ -60,27 +66,55 @@ mod java_defects4j_cli_16_option;
 #[cfg(test)]
 mod java_defects4j_cli_21_writeablecommandline;
 #[cfg(test)]
+mod java_defects4j_cli_27_optiongroup;
+#[cfg(test)]
 mod java_defects4j_cli_29_util;
 #[cfg(test)]
 mod java_defects4j_cli_5_util;
+#[cfg(test)]
+mod java_defects4j_closure_147_checkglobalthis;
 #[cfg(test)]
 mod java_defects4j_closure_165_recordtypebuilder;
 #[cfg(test)]
 mod java_defects4j_closure_28_inlinecostestimator;
 #[cfg(test)]
+mod java_defects4j_closure_80_nodeutil;
+#[cfg(test)]
 mod java_defects4j_codec_1_soundexutils;
+#[cfg(test)]
+mod java_defects4j_codec_8_base64inputstream;
+#[cfg(test)]
+mod java_defects4j_compress_33_deflatecompressorinputstream;
 #[cfg(test)]
 mod java_defects4j_compress_42_unixstat;
 #[cfg(test)]
 mod java_defects4j_compress_44_checksumcalculatinginputstream;
 #[cfg(test)]
+mod java_defects4j_csv_1_extendedbufferedreader;
+#[cfg(test)]
+mod java_defects4j_csv_2_csvrecord;
+#[cfg(test)]
+mod java_defects4j_gson_6_jsonadapterannotationtypeadapterfactory;
+#[cfg(test)]
+mod java_defects4j_jacksoncore_13_jsongeneratorimpl;
+#[cfg(test)]
 mod java_defects4j_jacksondatabind_105_jdkdeserializers;
 #[cfg(test)]
 mod java_defects4j_jacksondatabind_111_atomicreferencedeserializer;
 #[cfg(test)]
+mod java_defects4j_jacksondatabind_16_annotationmap;
+#[cfg(test)]
+mod java_defects4j_jacksondatabind_25_simpleabstracttyperesolver;
+#[cfg(test)]
+mod java_defects4j_jacksondatabind_34_numberserializer;
+#[cfg(test)]
 mod java_defects4j_jacksondatabind_39_nullifyingdeserializer;
 #[cfg(test)]
 mod java_defects4j_jacksondatabind_49_writableobjectid;
+#[cfg(test)]
+mod java_defects4j_jacksondatabind_79_objectidinfo;
+#[cfg(test)]
+mod java_defects4j_jacksondatabind_94_subtypevalidator;
 #[cfg(test)]
 mod java_defects4j_jsoup_16_documenttype;
 #[cfg(test)]
@@ -88,11 +122,19 @@ mod java_defects4j_jsoup_40_documenttype;
 #[cfg(test)]
 mod java_defects4j_jsoup_52_xmldeclaration;
 #[cfg(test)]
+mod java_defects4j_jsoup_56_documenttype;
+#[cfg(test)]
+mod java_defects4j_jsoup_79_leafnode;
+#[cfg(test)]
+mod java_defects4j_jsoup_86_comment;
+#[cfg(test)]
 mod java_defects4j_jsoup_91_uncheckedioexception;
 #[cfg(test)]
 mod java_defects4j_jsoup_92_parsesettings;
 #[cfg(test)]
 mod java_defects4j_jxpath_15_unioncontext;
+#[cfg(test)]
+mod java_defects4j_jxpath_18_attributecontext;
 #[cfg(test)]
 mod java_defects4j_jxpath_7_coreoperationgreaterthan;
 #[cfg(test)]
@@ -104,9 +146,35 @@ mod java_defects4j_jxpath_7_coreoperationlessthanorequal;
 #[cfg(test)]
 mod java_defects4j_jxpath_7_coreoperationrelationalexpression;
 #[cfg(test)]
+mod java_defects4j_lang_14_stringutils;
+#[cfg(test)]
+mod java_defects4j_lang_17_charsequencetranslator;
+#[cfg(test)]
+mod java_defects4j_lang_19_numericentityunescaper;
+#[cfg(test)]
 mod java_defects4j_lang_28_numericentityunescaper;
 #[cfg(test)]
+mod java_defects4j_lang_4_lookuptranslator;
+#[cfg(test)]
+mod java_defects4j_lang_64_valuedenum;
+#[cfg(test)]
+mod java_defects4j_lang_6_charsequencetranslator;
+#[cfg(test)]
+mod java_defects4j_math_103_normaldistributionimpl;
+#[cfg(test)]
 mod java_defects4j_math_14_weight;
+#[cfg(test)]
+mod java_defects4j_math_22_fdistribution;
+#[cfg(test)]
+mod java_defects4j_math_22_uniformrealdistribution;
+#[cfg(test)]
+mod java_defects4j_math_35_elitisticlistpopulation;
+#[cfg(test)]
+mod java_defects4j_math_6_baseoptimizer;
+#[cfg(test)]
+mod java_defects4j_math_70_bisectionsolver;
+#[cfg(test)]
+mod java_defects4j_math_95_fdistributionimpl;
 #[cfg(test)]
 mod java_defects4j_mockito_11_delegatingmethod;
 #[cfg(test)]
@@ -132,6 +200,16 @@ mod java_defects4j_mockito_29_same;
 #[cfg(test)]
 mod java_defects4j_mockito_2_timer;
 #[cfg(test)]
+mod java_defects4j_mockito_30_returnssmartnulls;
+#[cfg(test)]
+mod java_defects4j_mockito_31_returnssmartnulls;
+#[cfg(test)]
+mod java_defects4j_mockito_32_spyannotationengine;
+#[cfg(test)]
+mod java_defects4j_mockito_37_answersvalidator;
+#[cfg(test)]
 mod java_defects4j_mockito_38_argumentmatchingtool;
+#[cfg(test)]
+mod java_defects4j_mockito_5_verificationovertimeimpl;
 #[cfg(test)]
 mod java_defects4j_mockito_9_callsrealmethods;
