54401f714c100614c895edc44b6ab478c56aa3f0
Marko Ivankovic <marko@ivankovic.me>
2026-09-15 10:52:41 +0200

    Fmt

--- src/test/fixtures/defects4j/java_defects4j_cli_12_gnuparser.rs
+++ src/test/fixtures/defects4j/java_defects4j_cli_12_gnuparser.rs
@@ -18,8 +18,8 @@
 use anyhow::Result;
 
 use crate::test;
-use crate::test::helper::human_mapping::invariants::assert_ground_truth_invariants;
 use crate::test::helper::human_mapping::assert_matches_human_painting_within_limit;
+use crate::test::helper::human_mapping::invariants::assert_ground_truth_invariants;
 
 #[test]
 fn mapping() -> Result<()> {
